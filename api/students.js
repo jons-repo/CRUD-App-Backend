35df677ec9cd1d670ea6327b094f9df38cf37ed4
Kyaw Nanda <kyawnanda.lucky@gmail.com>
2023-07-02 03:04:02 -0400

    added route for addStudent

--- api/students.js
+++ api/students.js
@@ -30,8 +30,14 @@ router.get("/:id", async (req, res, next) => {
       ? res.status(200).json(singleStudent) 
       : res.status(404).send("Student Data Not Found")
   } catch (error) {
-
+    next(error);
   }
+});
+
+router.get("/addStudent", async (req, res, next) => {
+  //should try handling empty req body
+  const createStudent = await Student.create(req.body);
+  res.send(createStudent);
 });
 
 module.exports = router;
