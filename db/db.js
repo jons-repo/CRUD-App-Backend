1636bf9c943b627b7030c403928ef0b442a73bd6
jons-repo <s.jonathan4252@gmail.com>
2023-07-06 15:28:41 -0400

    ENV change.

--- db/db.js
+++ db/db.js
@@ -3,8 +3,7 @@ const { name } = require("../package.json");
 //name === campusesData
 //const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname')
 // knd = `postgres://knd:2782001knd@localhost:5433/${name}`
-// postgres://postgres:roman123@localhost:5432/${name}
-const db = new Sequelize(`postgres://default:XAnoxSlt0H4u@ep-delicate-sunset-041979.us-east-1.postgres.vercel-storage.com:5432/verceldb` + "?sslmode=require" , {
+const db = new Sequelize(process.env.POSTGRES_URL_NON_POOLING + "?sslmode=require" , {
   logging: false,
 });
 
