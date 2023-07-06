1ba282f47cba8f03ddcfc8caad7b4495256eda16
jons-repo <s.jonathan4252@gmail.com>
2023-07-06 15:20:39 -0400

    Starting deployment.

--- db/db.js
+++ db/db.js
@@ -4,7 +4,7 @@ const { name } = require("../package.json");
 //const sequelize = new Sequelize('postgres://user:pass@example.com:5432/dbname')
 // knd = `postgres://knd:2782001knd@localhost:5433/${name}`
 // postgres://postgres:roman123@localhost:5432/${name}
-const db = new Sequelize(`postgres://postgres:roman123@localhost:5432/${name}`, {
+const db = new Sequelize(`postgres://default:XAnoxSlt0H4u@ep-delicate-sunset-041979.us-east-1.postgres.vercel-storage.com:5432/verceldb` + "?sslmode=require" , {
   logging: false,
 });
 
